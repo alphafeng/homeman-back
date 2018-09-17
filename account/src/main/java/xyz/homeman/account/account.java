9d358bc94000e8f79cb87d8408cb16c076de84ef
keKe <13795209788@163.com>
2018-09-17 15:36:39 +0800

    add account model & jenkins file

---
+++ account/src/main/java/xyz/homeman/account/account.java
@@ -0,0 +1,13 @@
+package xyz.homeman.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class account {
+    public static void main(String[] args) {
+        SpringApplication.run(account.class, args);
+    }
+}
